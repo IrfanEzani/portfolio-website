3dfe15dff780b038dc9f1a4b6c2cf549a77afdc8
Irfan Ezani <irfanezani@gmail.com>
2024-01-21 01:33:12 -0500

    add files via upload

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Hero from "@/components/Main";
+import GlitcheLayout from "@/layouts/GlitcheLayout";
+const page = () => {
+  return (
+    <GlitcheLayout>
+      <Hero />
+    </GlitcheLayout>
+  );
+};
+export default page;
